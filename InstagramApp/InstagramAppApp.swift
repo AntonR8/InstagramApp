b3d29da96e588326189cd5317c24fee1d56342d2
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2024-11-21 13:32:27 +0300

    Initial Commit

---
+++ InstagramApp/InstagramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramAppApp.swift
+//  InstagramApp
+//
+//  Created by Антон Разгуляев on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
